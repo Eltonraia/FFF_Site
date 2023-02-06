058ade6b7d1c7d1077f8cc20e5062c07afb3147c
Eltonraia <elton14739@yahoo.com>
2023-02-05 21:34:53 -0600

    Reconfig site and added servises

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-//Toggler Button
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarItems = document.getElementsByClassName("nav-items")[0];
 
-toggleButton.addEventListener("click", () => {
-  navbarItems.classList.toggle("active");
-});
