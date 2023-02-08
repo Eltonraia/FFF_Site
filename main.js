8f14483e5d5c6731f860bb5be81b822bc14fc36b
Eltonraia <elton14739@yahoo.com>
2023-02-07 18:01:25 -0600

    complete giving page and add additional updates

--- main.js
+++ main.js
@@ -17,3 +17,25 @@ buttons.forEach((button) => {
     delete activeSlides.dataset.active;
   });
 });
+
+//Giving Page
+
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".giving-container");
+
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
